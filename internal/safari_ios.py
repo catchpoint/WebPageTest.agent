359c0587cd1fd4e5cb4b81f2825712a79942a7bc
Patrick Meenan <patmeenan@gmail.com>
2017-09-07 13:04:18 -0400

    Restored the task prep logic

--- internal/safari_ios.py
+++ internal/safari_ios.py
@@ -74,10 +74,10 @@ class iWptBrowser(object):
             recording = False
             while task['script'] and task['error'] is None and \
                     monotonic.monotonic() < end_time:
+                self.prepare_task(task)
                 command = task['script'].pop(0)
                 if not recording and command['record']:
                     recording = True
-                    self.prepare_task(task)
                     self.on_start_recording(task)
                 try:
                     self.process_command(command)
