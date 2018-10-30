20b5c8836d0beec9972206e511a733a620e38005
Stephen Donner <stephen.donner@gmail.com>
2018-10-30 16:42:55 -0700

    Fix pref-name for time_to_first_interactive in Firefox

--- internal/support/Firefox/profile/prefs.js
+++ internal/support/Firefox/profile/prefs.js
@@ -55,7 +55,7 @@ user_pref("dom.max_chrome_script_run_time", 0);
 user_pref("dom.max_script_run_time", 0);
 user_pref("dom.webnotifications.enabled", false);
 user_pref("dom.performance.time_to_dom_content_flushed.enabled", true);
-user_pref("dom.performance.time_to_interactive.enabled", true);
+user_pref("dom.performance.time_to_first_interactive.enabled", true);
 user_pref("dom.performance.time_to_non_blank_paint.enabled", true);
 user_pref("extensions.checkCompatibility", false);
 user_pref("extensions.pocket.enabled", false);
