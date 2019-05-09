9106fc42d08ab430c2b04becfa4a4c40624ee8a5
Patrick Meenan <patmeenan@gmail.com>
2019-05-09 18:35:05 -0400

    Added some test validation

--- internal/webpagetest.py
+++ internal/webpagetest.py
@@ -22,6 +22,10 @@ import ujson as json
 
 DEFAULT_JPEG_QUALITY = 30
 
+# Hard-fail any tests that include requests to URLs starting with one of these paths.
+BLOCK_URL_PATHS = [
+    "/latest/meta-data"
+]
 
 class WebPageTest(object):
     """Controller for interfacing with the WebPageTest server"""
@@ -1015,6 +1019,8 @@ class WebPageTest(object):
         logging.info('Uploading result')
         cpu_pct = None
         self.update_browser_viewport(task)
+        # Make sure we don't need to hard-fail this result
+        test_valid = self.validate_test_result(task)
         # Stop logging to the file
         if self.log_handler is not None:
             try:
@@ -1050,7 +1056,7 @@ class WebPageTest(object):
                 data['ec2zone'] = self.zone
             needs_zip = []
             zip_path = None
-            if os.path.isdir(task['dir']):
+            if test_valid and os.path.isdir(task['dir']):
                 # upload any video images
                 if bool(self.job['video']) and len(task['video_directories']):
                     for video_subdirectory in task['video_directories']:
@@ -1125,6 +1131,27 @@ class WebPageTest(object):
                 shutil.rmtree(self.workdir)
             except Exception:
                 pass
+
+    def validate_test_result(self, task):
+        """Check the test result for things that need to be blocked"""
+        is_valid = True
+        try:
+            requests_file = os.path.join(task['dir'], task['prefix'] + '_' + 'devtools_requests.json.gz')
+            if os.path.isfile(requests_file):
+                with gzip.open(requests_file, 'rb') as f_in:
+                    requests_data = json.load(f_in)
+                    if 'requests' in requests_data:
+                        for request in requests_data['requests']:
+                            if 'url' in request:
+                                for banned in BLOCK_URL_PATHS:
+                                    if request['url'].startswith(banned):
+                                        logging.debug('Failing test, attempted to fetch %s', request['url'])
+                                        is_valid = False
+                                if not is_valid:
+                                    break
+        except Exception:
+            pass
+        return is_valid
 
     def post_data(self, url, data, file_path, filename):
         """Send a multi-part post"""
