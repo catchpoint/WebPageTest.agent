7bb3be4b060acbf74c3948023d6b94c3a2d94625
Patrick Meenan <pmeenan@chromium.org>
2020-06-24 19:27:18 -0400

    Added a python 3 version of the alive script

---
+++ alive3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Watchdog helper script to auto-reboot the system if the agent stops working.
+# Add a test-binary entry to /etc/watchdog.conf and start the watchdog service:
+# test-binary = /home/ubuntu/wptagent/alive.sh
+
+: ${UBUNTU_VERSION:=`(lsb_release -rs | cut -b 1,2)`}
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+rm /var/log/watchdog/* || true
+python3 $DIR/alive.py --file /tmp/wptagent
