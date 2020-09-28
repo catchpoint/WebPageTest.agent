183396cc2020430307e12ea3750d82be153ac110
Wuyang Wang <wuwang@chromium.org>
2020-09-28 13:08:20 -0700

    Enable support Safari browser on MacOS
    Signed-off-by: Arthur Wang <wuwang@google.com>

--- wptagent.py
+++ wptagent.py
@@ -322,6 +322,10 @@ class WPTAgent(object):
         if platform.system() == "Windows":
             ret = self.requires('win32api', 'pywin32') and ret
 
+        if 'Safari' in detected_browsers and not self.options.iOS:
+            # if running for safari
+            ret = self.requires('selenium')
+
         # Optional imports
         self.requires('brotli')
         self.requires('fontTools', 'fonttools')
@@ -432,6 +436,8 @@ class WPTAgent(object):
                 print("Error configuring traffic shaping, make sure secure boot is disabled.")
             else:
                 print("Error configuring traffic shaping, make sure it is installed.")
+            # TODO(wuwang): Remove this comments if pfctl/dnctl works,
+            # comments below line for running in debugger
             ret = False
 
         # Update the Windows root certs
@@ -779,6 +785,10 @@ def find_browsers(options):
             browsers['Firefox Nightly'] = {'exe': nightly_path,
                                            'type': 'Firefox',
                                            'log_level': 5}
+        safari_path = '/Applications/Safari.app/Contents/MacOS/Safari'
+        if 'Safari' not in browsers and os.path.isfile(safari_path):
+            browsers['Safari'] = {'exe': safari_path, 'type': 'Safari'}
+
     logging.debug('Detected Browsers:')
     for browser in browsers:
         logging.debug('%s: %s', browser, browsers[browser]['exe'])
