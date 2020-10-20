cb6bac47dbe9c98ca93a689f9f0a71cbd59ce25b
Patrick Meenan <PatMeenan@gmail.com>
2020-10-20 12:06:23 -0700

    Update the microsoft cert to use a proxied domain

--- browser_install/edge.sh
+++ browser_install/edge.sh
@@ -7,7 +7,7 @@ do
     sleep 1
 done
 
-curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/microsoft.gpg add -
+curl -s https://www.webpagetest.org/keys/microsoft/microsoft.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/microsoft.gpg add -
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge-dev.list
 
 until sudo apt -y update
