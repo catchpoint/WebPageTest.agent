ac6abea25b7d0c67e1c0d60fff67e1a170f7a771
abdulsuhail <abdulsuhail471@gmail.com>
2021-09-02 21:25:40 +0530

    Header for Azion added

--- internal/optimization_checks.py
+++ internal/optimization_checks.py
@@ -250,6 +250,7 @@ class OptimizationChecks(object):
                        {'x-akamai-request-id': ''}],
             'Amazon CloudFront': [{'Via': 'CloudFront'}],
             'Aryaka': [{'X-Ar-Debug': ''}],
+            'Azion' : [{'Server' : 'Azion Technologies'}],
             'Baleen': [{'bln-version': ''}],
             'BelugaCDN': [{'Server': 'Beluga'},
                           {'X-Beluga-Cache-Status': ''}],
