8514350bc6e9ae16aea7d785936a6ab487072bf6
Patrick Meenan <PatMeenan@gmail.com>
2023-09-25 09:52:18 -0400

    Fixed the update script

--- update_wappalyzer.sh
+++ update_wappalyzer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-git clone --depth 1 -b master https://github.com/HTTPArchive/wappalyzer.git wappalyzer
+git clone --depth 1 -b main https://github.com/HTTPArchive/wappalyzer.git wappalyzer
 rm internal/support/Wappalyzer/technologies/*.json
 rm internal/support/Wappalyzer/*.json
 rm internal/support/Wappalyzer/wappalyzer.js
 cp wappalyzer/src/technologies/*.json internal/support/Wappalyzer/technologies/
 cp wappalyzer/src/*.json internal/support/Wappalyzer/
-cp wappalyzer/src/wappalyzer.js internal/support/Wappalyzer/
+cp wappalyzer/src/js/wappalyzer.js internal/support/Wappalyzer/
 rm -rf wappalyzer
