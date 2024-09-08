27449cc4778692643c8800c4b00dd35bbe219f59
Patrick Meenan <pmeenan@chromium.org>
2024-09-08 10:05:53 -0400

    Updated parsed_css schema

--- schema/all_parsed_css_pb2.py
+++ schema/all_parsed_css_pb2.py
@@ -14,13 +14,13 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14\x61ll_parsed_css.proto\"g\n\tCSSRecord\x12\x0c\n\x04\x64\x61te\x18\x01 \x02(\x05\x12\x0e\n\x06\x63lient\x18\x02 \x02(\t\x12\x0c\n\x04page\x18\x03 \x02(\t\x12\x14\n\x0cis_root_page\x18\x04 \x02(\x08\x12\x0b\n\x03url\x18\x05 \x02(\t\x12\x0b\n\x03\x63ss\x18\x06 \x02(\t')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14\x61ll_parsed_css.proto\"\x88\x01\n\tCSSRecord\x12\x0c\n\x04\x64\x61te\x18\x01 \x02(\x05\x12\x0e\n\x06\x63lient\x18\x02 \x02(\t\x12\x0c\n\x04page\x18\x03 \x02(\t\x12\x14\n\x0cis_root_page\x18\x04 \x02(\x08\x12\x11\n\troot_page\x18\x05 \x02(\t\x12\x0c\n\x04rank\x18\x06 \x01(\x05\x12\x0b\n\x03url\x18\x07 \x02(\t\x12\x0b\n\x03\x63ss\x18\x08 \x02(\t')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'all_parsed_css_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
-  _globals['_CSSRECORD']._serialized_start=24
-  _globals['_CSSRECORD']._serialized_end=127
+  _globals['_CSSRECORD']._serialized_start=25
+  _globals['_CSSRECORD']._serialized_end=161
 # @@protoc_insertion_point(module_scope)
